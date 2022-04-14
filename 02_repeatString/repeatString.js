fa9a13883262a5b5e284baaa8668f7d6224c59a7
Boban Kilvin <btrajcevski10@gmail.com>
2022-04-14 04:44:23 -0400

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+	let repeatedString = '';
+	
+	if (repeat < 0) return "ERROR";
 
+	for (let i = 0; i < repeat; i++) {
+		repeatedString += string;
+	}
+
+	return repeatedString;
 };
 
 // Do not edit below this line
