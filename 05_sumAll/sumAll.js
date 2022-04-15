281d756c57f4d101d24471c5cb5c8f3ae70fb325
Boban Kilvin <btrajcevski10@gmail.com>
2022-04-15 17:11:07 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	let sum = 0;
 
+	if ((typeof start !== "number" || typeof end !== "number") ||
+			(start < 0 || end < 0)) {
+		return 'ERROR';
+	}	
+	
+	if (start > end) {
+		let temp = start;
+		start = end;
+		end = temp;
+	}
+	
+	for (let i = start;i <= end; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
