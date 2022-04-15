724e43bcc0c93d5bac9d7946a9df7ad1a7ffc6ef
Boban Kilvin <btrajcevski10@gmail.com>
2022-04-15 16:17:23 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+	let args = Array.prototype.slice.call(arguments, 1);
 
+	return arr.filter(element => !args.includes(element));
 };
 
 // Do not edit below this line
