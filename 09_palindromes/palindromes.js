0fde71a543ad01732af9f78ce42a87b67e399e5a
Boban Kilvin <btrajcevski10@gmail.com>
2022-05-09 04:06:36 -0400

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	let newStr = str.replace(/[\W_\s]/gi, '').toLowerCase();
 
+	return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
