af5ecb3ee72fde79737b0193b3905dd4f46fb633
Boban Kilvin <btrajcevski10@gmail.com>
2022-05-10 03:39:01 -0400

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	return people.reduce((oldest, current) => {
+		let currentYear = new Date().getFullYear();
+		let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+		let currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+		
+		return oldestAge > currentAge ? oldest : current;
+	});
 };
 
 // Do not edit below this line
